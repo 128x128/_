c677762ebd2a4abf48d71ba4e8f6cdb219a266c6
128x128 <adamzzzhao@gmail.com>
2022-12-26 17:47:53 +1100

    yeeehaaaw

--- node.py
+++ node.py
@@ -5,45 +5,67 @@ random.seed(1337)
 
 ALL = []
 
+class Variable: 
+	def __init__(self): self._ = None
+	def __str__(self): return f"{self._.id} = {self.__repr__()}" 
+	def __repr__(self): x=f"{format(self._.data,'.4f')}"; return " "+x if self._.data>0 else x;
+
 class Node:
 
-	def __init__(self, id=None, data=0, meta=[], type=None):
+	def __init__(self, id="Variable", data=0, type=Variable()):
 		self.id   = id
-		self.data = data  
+		self.data = data
 		self.type = type
-		for x in meta: setattr(self, x[0], x[1])
 		ALL.append(self)
 
-	def istype(t): return self.type==t
-	def rand(a=-1,b=1): return Node(data=random.uniform(a, b))
+	def new(id="Variable", data=0, type=Variable()): n=Node(id=id, data=data, type=type); n.type._=n; return n
+	# def rand(a=-1,b=1): return Node.new(data=random.uniform(a, b))
+	def Tensor(*shape): return Node.new(id="Tensor", data=[Node.new() for _ in range(math.prod(shape))], type=Tensor(shape))
 
-	def Tensor(*shape): return Node(id="Tensor", data=[Node.rand() for _ in range(math.prod(shape))], meta=[('shape', shape)], type=Tensor)
-
-	def __str__(self):
-		if (self.type!=None): return self.type.str(self)
-		return f"+{format(self.data, '.4f')}" if self.data>0 else f"{format(self.data, '.4f')}" 
-
-	def __repr__(self):
-		if (self.type!=None): return self.type.str(self)
-		return f"+{format(self.data, '.4f')}" if self.data>0 else f"{format(self.data, '.4f')}" 
+	def __str__(self): return self.type.__str__()
+	def __repr__(self): return self.type.__repr__()
 
 class Function:
-	def __init__(self, id=None, out=None):
-		self.id = out
+	def __init__(self, id=None, type=None):
+		self.id = id
+
+class Unary:
+	def __init__(self, x, y):
+		self.x = x
+		self.y = y
+class Binary:
+	def __init__(self, x, y, z):
+		self.x = x
+		self.y = y
+		self.z = z
+class Reduce:
+	def __init__(self, x, y):
+		self.x = x
+		self.y = y
+
 
 class Tensor: 
-	  def nmat(self): return math.prod(self.shape[0:-2])
-	  def nvec(self): return math.prod(self.shape[0:-1])
-	  def mxn(self): return self.shape[-1]*self.shape[-2]
-	  def vec(self, i): return [self.data[_+i*self.shape[-1]] for _ in range(self.shape[-1])]
-	  def mat(self, i): return [Tensor.vec(self, i*self.shape[-2]+_) for _ in range(self.shape[-2])]
-	  def indent(self, v, i): s=self.id+":"; v.insert(0, s) if i == 0 else v.insert(0, len(s)*" "); return v
-	  def vecstr(self, i): return " ".join( Tensor.indent(self, [str(self.data[_+i*self.shape[-1]]) for _ in range(self.shape[-1])], i) )
-	  def matstr(self, i): return "\n".join( [Tensor.vecstr(self, i*self.shape[-2]+_) for _ in range(self.shape[-2])] )
-	  def str(self): return "\n\n".join([Tensor.matstr(self, _) for _ in range(Tensor.nmat(self))]) 
-			   
+	def __init__(self, shape): self._ = None; self.shape = shape 
+	def nmat(self): return math.prod(self.shape[0:-2])
+	def nvec(self): return math.prod(self.shape[0:-1])
+	def mxn(self): return self.shape[-2]*self.shape[-1]
+
+	def vec(self, i): return [self._.data[_+i*self.shape[-1]] for _ in range(self.shape[-1])]
+	def mat(self, i): return [self.vec(i*self.shape[-2]+_) for _ in range(self.shape[-2])]
+
+	def indent(self, v, i): s=self._.id+":"; v.insert(0, s) if i == 0 else v.insert(0, len(s)*" "); return v
+	def vecstr(self, i): return " ".join( self.indent([self._.data[_+i*self.shape[-1]].__repr__() for _ in range(self.shape[-1])], i) )
+	def matstr(self, i): return "\n".join( [self.vecstr(i*self.shape[-2]+_) for _ in range(self.shape[-2])] )
+
+	def __str__(self): return "\n\n".join([self.matstr(_) for _ in range(self.nmat())]) 
+	def __repr__(self): return self._.id + str(self.shape);
+
+	def reshape(self, *shape): self.shape = shape; return self.shape
+	def transpose(self): s=list(self.shape); s[-1]=self.shape[-2]; s[-2]=self.shape[-1]; return tuple(s)
+	def flatten(self): self.shape = (math.prod(self.shape), 1); return self.shape
 
 
-x = Node.Tensor(5, 32, 16)
+# x = Node.new()
+# print(x)
+x = Node.Tensor(4, 2, 3)
 print(x)
-
