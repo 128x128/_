fbb8a820bc9b70e4b4957e6f62118c435be956a7
128x128 <adamzzzhao@gmail.com>
2022-12-27 23:37:29 +1100

    1

--- node.py
+++ node.py
@@ -3,49 +3,51 @@ import math
 
 random.seed(1337) 
 
-ALL = []
 
 class Variable: 
 	def __init__(self): self._ = None
 	def __str__(self): return f"{self._.id} = {self.__repr__()}" 
 	def __repr__(self): x=f"{format(self._.data,'.4f')}"; return " "+x if self._.data>0 else x;
+	def __add__(x, y): return Node.new(data=x._.data+y._.data, type=Variable())
 
 class Node:
 
-	def __init__(self, id="Variable", data=0, type=Variable()):
+	def __init__(self, id="Variable", data=None, type=None, fn=None):
 		self.id   = id
 		self.data = data
 		self.type = type
-		ALL.append(self)
+		self.fn   = fn
 
-	def new(id="Variable", data=0, type=Variable()): n=Node(id=id, data=data, type=type); n.type._=n; return n
-	# def rand(a=-1,b=1): return Node.new(data=random.uniform(a, b))
-	def Tensor(*shape): return Node.new(id="Tensor", data=[Node.new() for _ in range(math.prod(shape))], type=Tensor(shape))
+	def new(id=None, data=None, type=None): n=Node(id=id, data=data, type=type); n.type._=n; return n
+	def link(): 
+
+	def rand(): return Node.Variable(random.uniform(-1, 1))
+	def zero(): return Node.Variable(0)
+	def one(): return Node.Variable(1)
+
+	def Variable(data): return Node.new(id="Variable", data=data, type=Variable())
+	def Tensor(*shape): return Node.new(id="Tensor", data=[Node.rand() for _ in range(math.prod(shape))], type=Tensor(shape))
+	def Binary(x, y): return Node.new(id="Binary", type=Binary(Binary.add, x, y, z))
 
 	def __str__(self): return self.type.__str__()
 	def __repr__(self): return self.type.__repr__()
 
-class Function:
-	def __init__(self, id=None, type=None):
-		self.id = id
+	def __add__(x, y): return Node.new(id=f"{x.id} + {y.id}", fn=Node.Binary(x, y), type=(x.type + y.type))
+
+# class Function:
+	# def __init__(self, x, y, out, type): self.type = type; self.x = x; self.y = y; self.out = out
+	# def apply(self): 
 
 class Unary:
-	def __init__(self, x, y):
-		self.x = x
-		self.y = y
-class Binary:
-	def __init__(self, x, y, z):
-		self.x = x
-		self.y = y
-		self.z = z
-class Reduce:
-	def __init__(self, x, y):
-		self.x = x
-		self.y = y
+	def __init__(self, _, x, out): self.x = x; self.y = y; self.out = out
 
+class Binary:
+	def __init__(self, _, x, y, out): self.x = x; self.y = y; self.out = out
+	def add(x, y, out): out._.data = x._.data + y._.data
 
 class Tensor: 
 	def __init__(self, shape): self._ = None; self.shape = shape 
+
 	def nmat(self): return math.prod(self.shape[0:-2])
 	def nvec(self): return math.prod(self.shape[0:-1])
 	def mxn(self): return self.shape[-2]*self.shape[-1]
@@ -65,7 +67,9 @@ class Tensor:
 	def flatten(self): self.shape = (math.prod(self.shape), 1); return self.shape
 
 
-# x = Node.new()
+# x = Node.Tensor(2, 3)
 # print(x)
-x = Node.Tensor(4, 2, 3)
-print(x)
+x = Node.rand()  
+y = Node.rand()  
+z = x + y
+print(z)
