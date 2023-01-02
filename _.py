d67e66651f30fd975d0b7000e6b1f276f35ef6ea
128x128 <adamzzzhao@gmail.com>
2023-01-03 01:05:57 +1100

    matmul

--- _.py
+++ _.py
@@ -17,7 +17,8 @@ class Node:
 	# printer
 	def indent(self, buffer:str, n:int): return buffer.replace("\n", "\n"+" "*n)
 	def label(self, buffer:list): s=self.id+" = "; buffer.insert(0, s); return self.indent("".join(buffer), len(s))
-	def format(self, buffer:list): return "".join(buffer) if self.id==None else self.label(buffer)
+	def colorise(self, buffer:str): return f"\033[95m{buffer}\033[0m"
+	def format(self, buffer:list): return "".join(buffer) if self.id==None else self.colorise(self.label(buffer))
 	
 	def push(self, t, e): temp = list(t); temp.append(e); return tuple(temp)
 
@@ -30,44 +31,57 @@ class Node:
 
 class Function(Node):
 	def __init__(self, *fxn, op=None): 
-		self.data = self.push(fxn,fxn[0].type()) 
+		self.data = fxn 
 		self.op = op
-		self.__call__()
 	@property
 	def ret(self): return self.data[-1]
-	def __call__(self): self.op(self.data)
+	def apply(self, ret): 
+		self.data = self.push(self.data, ret) 
+		self.ret.ctx = self
+		if isinstance(self.ret, Tensor): self.ret.permuate(self)
+		if isinstance(self.ret, Variable): self.__call__()
+		return self.ret
+	def __call__(self): 
+		self.op(self.data) # invoke function
+	def __str__(self):
+		return f"F: {self.data}"
 
 class Variable(Node):
 	def __init__(self, data=0): self.data = data; self.type = Variable
 	def __str__(self): return f"{self.id} = {self.__repr__()}" if self.id!=None else f"{self.__repr__()}" 
-	def __repr__(self): x=f"{format(self.data,'.4f')}"; return "+"+x if self.data>0 else x;
+	def __clr__(self, x:str): return f"\033[92m+{x}\033[0m" if self.data>0 else f"\033[91m{x}\033[0m"
+	def __repr__(self): return self.__clr__(f"{format(self.data,'.4f')}")
 	def __call__(self): self.ctx(); return self
 
-	@staticmethod
+
+	# unary
+	def exp(self): return None
+	def relu(self): return None
+	def sigmoid(self): return None
+
+	# binary 
 	def cadd(_): _[-1].data=_[0].data+_[1].data
-	@staticmethod
 	def csub(_): _[-1].data=_[0].data-_[1].data
-	@staticmethod
 	def cmul(_): _[-1].data=_[0].data*_[1].data
-	@staticmethod
 	def cdiv(_): _[-1].data=_[0].data/_[1].data
-	def add(x:Variable, y:Variable): return Function(x, y, op=Variable.cadd).ret
-	def sub(x:Variable, y:Variable): return Function(x, y, op=Variable.csub)
-	def mul(x:Variable, y:Variable): return Function(x, y, op=Variable.cmul)
-	def div(x:Variable, y:Variable): return Function(x, y, op=Variable.cdiv)
+	def add(x, y): return Function(x, y, op=Variable.cadd).apply(Variable())
+	def sub(x, y): return Function(x, y, op=Variable.csub).apply(Variable())
+	def mul(x, y): return Function(x, y, op=Variable.cmul).apply(Variable())
+	def div(x, y): return Function(x, y, op=Variable.cdiv).apply(Variable())
+
+	def csum(_): _[-1].data=sum([x.data for x in _[0].data])
+	def cmax(_): _[-1].data=max([x.data for x in _[0].data])
+	def cmin(_): _[-1].data=min([x.data for x in _[0].data])
 
 class Tensor(Node):
-	def __init__(self, shape, data=None): 
-		self.data = data
-		self.shape = self.reshape(shape)
-
-	# Initializers
-	@staticmethod
+	def __init__(self, shape, data=None): self.data = data; self.shape = self.reshape(shape)
+	# initializers
+	def empty(*shape):  return Tensor(shape, data=[])
+	def fill(*shape):  return Tensor(shape, data=[None  for _ in range(math.prod(shape))])
 	def ones(*shape):  return Tensor(shape, data=[Variable(1) for _ in range(math.prod(shape))])
-	@staticmethod
 	def zeros(*shape): return Tensor(shape, data=[Variable(0) for _ in range(math.prod(shape))])
-	@staticmethod
 	def randn(*shape): return Tensor(shape, data=[Variable(random.uniform(-1, 1)) for _ in range(math.prod(shape))])
+	def arange(*shape): return Tensor(shape, data=[Variable(_+1) for _ in range(math.prod(shape))])
 
 	# tensor shape ops
 	def restride(self): return tuple([math.prod(self.shape[_+1:len(self.shape)]) for _ in range(len(self.shape))])
@@ -78,48 +92,68 @@ class Tensor(Node):
 	def nmat(self): return math.prod(self.shape[0:-2])
 	def nvec(self): return math.prod(self.shape[0:-1])
 	def mxn(self): return self.shape[-2]*self.shape[-1]
-	def idx(self, x): return x if isinstance(x, int) else sum([x[-(_+1)]*self.stride[-(_+1)] for _ in range(len(x))])
+	def idx(self, *i): return i if isinstance(i, int) else sum([i[-(_+1)]*self.stride[-(_+1)] for _ in range(len(i))])
 
-	def vec(self, i): return [self.data[_+i*self.shape[-1]] for _ in range(self.shape[-1])]
-	def mat(self, i): return [self.vec(i*self.shape[-2]+_) for _ in range(self.shape[-2])]
+	def matidx(self, i:int): return self.mxn()*i
+	def vec(self, i:int): return [self.data[_+i*self.shape[-1]] for _ in range(self.shape[-1])]
+	def mat(self, i:int): return [self.vec(i*self.shape[-2]+_) for _ in range(self.shape[-2])]
+	def matrow(self, v:int, i:int): return [self.data[self.matidx(v)+(i*self.shape[-1])+_] for _ in range(self.shape[-1]) ]
+	def matcol(self, v:int, i:int): return [self.data[self.matidx(v)+(self.shape[-1]*_)+i] for _ in range(self.shape[-2]) ]
 
 	def vecstr(self, i:int): return " ".join( [self.data[_+i*self.shape[-1]].__repr__() for _ in range(self.shape[-1])])
 	def matstr(self, i:int): return "\n".join( [self.vecstr(i*self.shape[-2]+_) for _ in range(self.shape[-2])])
 
 	def __str__(self): return self.format(list("\n\n".join([self.matstr(_) for _ in range(self.nmat())])+"\n"))
 	def __repr__(self): return self.id + str(self.shape);
+	def __getitem__(self, i): return self.data[self.idx(i)]
+
+	# tensor compute ops
+	def broadcast(self, data):
+		for _ in range(len(data)): self.data[_] = data[_]
+	def permuate(self, fn=None):
+		if (fn.op==Variable.cadd): self.broadcast([fn.data[0].data[_]+fn.data[1].data[_] for _ in range(len(self.data))])
+		if (fn.op==Variable.csub): self.broadcast([fn.data[0].data[_]-fn.data[1].data[_] for _ in range(len(self.data))])
+		if (fn.op==Variable.cmul): self.tenmul(fn.data[0], fn.data[1])
+	def __call__(x): 
+		for _ in x[-1].data: _()
 
 	# UNARY OPS
-	def relu(self): return None
-	def exp(self): return None
 	def softmax(self): return None
-	def sigmoid(self): return None
 
 	# BINARY OPS
-	def add(self): return None
-	def sub(self): return None
-	def mul(self): return None
+	def add(x, y): return Function(x, y, op=Variable.cadd).apply(Tensor.fill(*x.shape))
+	def sub(x, y): return Function(x, y, op=Variable.csub).apply(Tensor.fill(*x.shape))
+	def mul(x, y): return Function(x, y, op=Variable.cmul).apply(Tensor.fill(*x.mulshape(y)))
+
+	def mulshape(x, y): _=list(x.shape[0:-2]); _.append(x.shape[-2]); _.append(y.shape[-1]); return tuple(_)
+	def matmul(self, x, y, _):
+		for i in range(x.shape[-2]):
+			for j in range(y.shape[-1]):
+				T = Tensor.fill(y.shape[-2], 1);
+				T.broadcast([v[0]*v[1] for v in zip(x.matrow(_, i), y.matcol(_, j))])	
+				if len(self.shape)==2: self.data[self.idx(i,j)] = T.sum()  
+				else: self.data[self.idx(_,i,j)] = T.sum() 
+	def tenmul(self, x, y):
+		for _ in range(self.nmat()): self.matmul(x, y, _)
 
 	# REDUCE OPS
-	def sum(self): return None
-	def min(self): return None
-	def max(self): return None
+	def sum(self): return Function(self, op=Variable.csum).apply(Variable())
+	def min(self): return Function(self, op=Variable.cmin).apply(Variable())
+	def max(self): return Function(self, op=Variable.cmax).apply(Variable())
 
-	# def __getitem__(self, x): return self._.data[x] if isinstance(x, int) else self._.data[sum([x[-(_+1)]*self.stride[-(_+1)] for _ in range(len(x))])]
 
-	
-	# def mulshape(a, b): _=list(a.shape[0:-2]); _.append(a.shape[-2]); _.append(b.shape[-1]); return tuple(_)
-	# def matmul(self, v, a, b):
-		# m,k,n = a.type.shape[-1], b.type.shape[-2], b.type.shape[-1] 
-		# for i in range(m):
-			# for j in range(n):
-				# self._.data[self.idx(v, i, j)] = 
-		# return self
 
-	# def tenmul(a, b): return [sum([a._.data[0]*a._.data[0] for _ in range(a.shape)]) for _ in range(math.prod(a.mulshape(b)))]
 
-x = Variable(1)
-y = Variable(3)
-z = x + y
+
+x = Tensor.randn(3, 4)
+y = Tensor.randn(4, 3)
+
+z = x * y
+
+x.id = "x"
+y.id = "y"
+z.id = "z"
+
+print(x)
+print(y)
 print(z)
-
