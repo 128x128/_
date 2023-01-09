a9ab3bfa25ffe6d41a2e710749ea8c44b3715eef
128x128 <adamzzzhao@gmail.com>
2023-01-09 20:26:05 +1100

    refactoring

--- _.py
+++ _.py
@@ -1,117 +1,83 @@
 # import random
 import math, random, statistics
+import numpy as np
 from typing import List, Tuple, Callable, Optional
 
 random.seed(1337) 
 
-class Node:
-	id   = None
-	data = None
-	type = type
-	ctx  = None
+# class Variable(Node):
+	# def __init__(self, data=0): self.data = data; self.type = Variable
+	# def __str__(self): return f"{self.id} = {self.__repr__()}" if self.id!=None else f"{self.__repr__()}" 
+	# def __clr__(self, x:str): return f"\033[92m+{x}\033[0m" if self.data>0 else f"\033[91m{x}\033[0m"
+	# def __repr__(self): return self.__clr__(f"{format(self.data,'.4f')}")
+	# def __call__(self): self.ctx(); return self
 
-	# output format
+
+	# # Unary: (Variable) -> Variable
+	# def cneg(_): _[-1].data=_[0].data*-1
+	# def cexp(_): _[-1].data=math.exp(_[0].data)
+	# def csqrt(_): _[-1].data=math.sqrt(_[0].data)
+	# def crelu(_): _[-1].data=math.exp(_[0].data)
+	# def csigmoid(_): _[-1].data=math.exp(_[0].data)
+
+	# def neg(self): return Function(x,op=Variable.cneg).apply(Variable())
+	# def exp(self): return Function(x,op=Variable.cexp).apply(Variable())
+	# def sqrt(self): return Function(x,op=Variable.csqrt).apply(Variable())
+	# def relu(self): return Function(x,op=Variable.crelu).apply(Variable())
+	# def sigmoid(self): return Function(x,op=Variable.csigmoid).apply(Variable())
+
+	# # Binary: (Variable x Variable) -> Variable
+	# def cadd(_): _[-1].data=_[0].data+_[1].data
+	# def csub(_): _[-1].data=_[0].data-_[1].data
+	# def cmul(_): _[-1].data=_[0].data*_[1].data
+	# def cdiv(_): _[-1].data=_[0].data/_[1].data
+	# def cpow(_): _[-1].data=math.pow(_[0].data, _[1].data)
+
+	# def add(x, y): return Function(x, y, op=Variable.cadd).apply(Variable())
+	# def sub(x, y): return Function(x, y, op=Variable.csub).apply(Variable())
+	# def mul(x, y): return Function(x, y, op=Variable.cmul).apply(Variable())
+	# def div(x, y): return Function(x, y, op=Variable.cdiv).apply(Variable())
+	# def pow(x, y): return Function(x, y, op=Variable.cpow).apply(Variable())
+
+	# # Reduce: Tensor -> Variable
+	# def csum(_): _[-1].data=sum([x.data for x in _[0].data])
+	# def cmax(_): _[-1].data=max([x.data for x in _[0].data])
+	# def cmin(_): _[-1].data=min([x.data for x in _[0].data])
+	# def cavg(_): _[-1].data=statistics.avg([x.data for x in _[0].data])
+	# def cmean(_): _[-1].data=statistics.mean([x.data for x in _[0].data])
+
+
+class Tensor():
+	def __init__(self, shape=(1,1), data=None, dtype=None): 
+		id         = None
+		self.data  = data 
+		self.shape = self.reshape(shape)
+		self.dtype = dtype
+		ctx        = None
+
+	
+	# initializers
+	@staticmethod
+	def fill(*shape):  return Tensor(shape, data=[None  for _ in range(math.prod(shape))], dtype=float)
+	@staticmethod
+	def ones(*shape):  return Tensor(shape, data=[1 for _ in range(math.prod(shape))], dtype=float)
+	@staticmethod
+	def zeros(*shape): return Tensor(shape, data=[0 for _ in range(math.prod(shape))], dtype=float)
+	@staticmethod
+	def randn(*shape): return Tensor(shape, data=[random.uniform(-1, 1) for _ in range(math.prod(shape))], dtype=float)
+	@staticmethod
+	def arange(*shape): return Tensor(shape, data=[_+1 for _ in range(math.prod(shape))], dtype=float)
+	@staticmethod
+	def op(*shape): return Tensor(shape, data=[Function() for _ in range(math.prod(shape))], dtype=Function)
+	@staticmethod
+	def str(*shape): return Tensor(shape, data=[" " for _ in range(math.prod(shape))], dtype=str)
+
 	def indent(self, buffer:str, n:int): return buffer.replace("\n", "\n"+" "*n)
 	def label(self, buffer:list): s=self.id+" = "; buffer.insert(0, s); return self.indent("".join(buffer), len(s))
-	def colorise(self, buffer:str): return f"\033[95m{buffer}\033[0m"
-	def format(self, buffer:list): return "".join(buffer) if self.id==None else self.colorise(self.label(buffer))
+	def format(self, buffer:list): return "".join(buffer) if self.id==None else self.label(buffer)
 
 	# helper methods
 	def push(self, t, e): temp = list(t); temp.append(e); return tuple(temp)
-
-	# ops
-	def __neg__(x):        return x.neg()
-	def __add__(x, y):     return x.add(y)
-	def __sub__(x, y):     return x.sub(y)
-	def __mul__(x, y):     return x.mul(y) 
-	def __truediv__(x, y): return x.div(y) 
-
-
-class Function(Node):
-	def __init__(self, *fxn, op=None): self.data = fxn; self.op = op
-	@property
-	def ret(self): return self.data[-1]
-	@property
-	def name(self): return f"\033[96m{self.op.__name__}\033[0m"
-	def apply(self, ret): 
-		self.data = self.push(self.data, ret) 
-		self.ret.ctx = self
-		if isinstance(self.ret, Tensor): self.ret.permuate(op=self.op, x=self.data[0], y=self.data[1])
-		if isinstance(self.ret, Variable): self.__call__()
-		return self.ret
-	def __call__(self): 
-		self.op(self.data)
-		# print(self.__repr__())
-	
-	def __repr__(self): 
-		if (len(self.data)==2): return f"{self.name} ( {self.data[0].id} ) -> {self.data[1].id}"
-		if (len(self.data)==3): return f"{self.name} ( {self.data[0].id} {self.data[1].id} ) -> {self.data[2].id}"
-	def __str__(self): 
-		if (len(self.data)==2): return f"{self.name}( x={self.data[0].id} ):\n{self.data[0]} \n= {self.data[1]}\n"
-		if (len(self.data)==3): return f"{self.name}( x={self.data[0].id}, y={self.data[1].id} ):\n{self.data[0]}, \n{self.data[1]} \n= {self.data[2]}\n"
-
-class Variable(Node):
-	def __init__(self, data=0): self.data = data; self.type = Variable
-	def __str__(self): return f"{self.id} = {self.__repr__()}" if self.id!=None else f"{self.__repr__()}" 
-	def __clr__(self, x:str): return f"\033[92m+{x}\033[0m" if self.data>0 else f"\033[91m{x}\033[0m"
-	def __repr__(self): return self.__clr__(f"{format(self.data,'.4f')}")
-	def __call__(self): self.ctx(); return self
-
-
-	# Unary: (Variable) -> Variable
-	def cneg(_): _[-1].data=_[0].data*-1
-	def cexp(_): _[-1].data=math.exp(_[0].data)
-	def csqrt(_): _[-1].data=math.sqrt(_[0].data)
-	def crelu(_): _[-1].data=math.exp(_[0].data)
-	def csigmoid(_): _[-1].data=math.exp(_[0].data)
-
-	def neg(self): return Function(x,op=Variable.cneg).apply(Variable())
-	def exp(self): return Function(x,op=Variable.cexp).apply(Variable())
-	def sqrt(self): return Function(x,op=Variable.csqrt).apply(Variable())
-	def relu(self): return Function(x,op=Variable.crelu).apply(Variable())
-	def sigmoid(self): return Function(x,op=Variable.csigmoid).apply(Variable())
-
-	# Binary: (Variable x Variable) -> Variable
-	def cadd(_): _[-1].data=_[0].data+_[1].data
-	def csub(_): _[-1].data=_[0].data-_[1].data
-	def cmul(_): _[-1].data=_[0].data*_[1].data
-	def cdiv(_): _[-1].data=_[0].data/_[1].data
-	def cpow(_): _[-1].data=math.pow(_[0].data, _[1].data)
-
-	def add(x, y): return Function(x, y, op=Variable.cadd).apply(Variable())
-	def sub(x, y): return Function(x, y, op=Variable.csub).apply(Variable())
-	def mul(x, y): return Function(x, y, op=Variable.cmul).apply(Variable())
-	def div(x, y): return Function(x, y, op=Variable.cdiv).apply(Variable())
-	def pow(x, y): return Function(x, y, op=Variable.cpow).apply(Variable())
-
-	# Reduce: Tensor -> Variable
-	def csum(_): _[-1].data=sum([x.data for x in _[0].data])
-	def cmax(_): _[-1].data=max([x.data for x in _[0].data])
-	def cmin(_): _[-1].data=min([x.data for x in _[0].data])
-	def cavg(_): _[-1].data=statistics.avg([x.data for x in _[0].data])
-	def cmean(_): _[-1].data=statistics.mean([x.data for x in _[0].data])
-
-
-	
-
-class Tensor(Node):
-	def __init__(self, shape, data=None, dtype=None): self.data = data; self.shape = self.reshape(shape); self.dtype=dtype
-
-	# initializers
-	@staticmethod
-	def empty(*shape): return Tensor(shape, data=[], dtype=None)
-	@staticmethod
-	def fill(*shape):  return Tensor(shape, data=[None  for _ in range(math.prod(shape))], dtype=Variable)
-	@staticmethod
-	def ones(*shape):  return Tensor(shape, data=[Variable(1) for _ in range(math.prod(shape))], dtype=Variable)
-	@staticmethod
-	def zeros(*shape): return Tensor(shape, data=[Variable(0) for _ in range(math.prod(shape))], dtype=Variable)
-	@staticmethod
-	def randn(*shape): return Tensor(shape, data=[Variable(random.uniform(-1, 1)) for _ in range(math.prod(shape))], dtype=Variable)
-	@staticmethod
-	def arange(*shape): return Tensor(shape, data=[Variable(_+1) for _ in range(math.prod(shape))], dtype=Variable)
-	@staticmethod
-	def strbuff(*shape): return Tensor(shape, data=["+" for _ in range(math.prod(shape))], dtype=str).inter_replace(shape[1], "\n")
 
 	# tensor shape ops
 	def restride(self): return tuple([math.prod(self.shape[_+1:len(self.shape)]) for _ in range(self.rank)])
@@ -139,7 +105,7 @@ class Tensor(Node):
 
 	def __uid__(self): return self.format(list("\n\n".join([self.matuid(_) for _ in range(self.nmat)])+"\n"))
 	def __str__(self): 
-		if self.dtype==str: return "".join([_ for _ in self.data])
+		if self.dtype==Char: return "".join([_.data for _ in self.data])
 		if self.dtype==Variable: return self.format(list("\n\n".join([self.matstr(_) for _ in range(self.nmat)])+"\n"))
 	def __repr__(self): return self.id + str(self.shape);
 	def __getitem__(self, i): return self.data[self.idx(i)]
@@ -176,9 +142,9 @@ class Tensor(Node):
 	def maxpool2d(self): return None
 
 	# BINARY OPS
-	def add(x, y): return Function(x, y, op=Variable.cadd).apply(Tensor.fill(*x.shape))
-	def sub(x, y): return Function(x, y, op=Variable.csub).apply(Tensor.fill(*x.shape))
-	def mul(x, y): return Function(x, y, op=Variable.cmul).apply(Tensor.fill(*x.mulshape(y)))
+	def add(x, y): return Function(x, y, op=_add).apply(Tensor.fill(*x.shape))
+	def sub(x, y): return Function(x, y, op=_sub).apply(Tensor.fill(*x.shape))
+	def mul(x, y): return Function(x, y, op=_cmul).apply(Tensor.fill(*x.mulshape(y)))
 
 	def mulshape(x, y): _=list(x.shape[0:-2]); _.append(x.shape[-2]); _.append(y.shape[-1]); return tuple(_)
 	def matmul(self, x, y, _):
@@ -187,28 +153,29 @@ class Tensor(Node):
 				T = Tensor.fill(y.shape[-2], 1);
 				T.id = f"matmulKernal{[i, j]}"
 				T.cast([v[0]*v[1] for v in zip(x.matrow(_, i), y.matcol(_, j))])
-				if len(self.shape)==2: self.data[self.idx(i,j)] = T.sum()  
-				else: self.data[self.idx(_,i,j)] = T.sum() 
+				if len(self.shape)==2: self.data[self.idx(i,j)] = T.sum()
+				else: self.data[self.idx(_,i,j)] = T.sum()
 	def tenmul(self, x, y):
 		for _ in range(self.nmat): self.matmul(x, y, _)
 
 	# REDUCE OPS
-	def sum(self): return Function(self, op=Variable.csum).apply(Variable())
-	def min(self): return Function(self, op=Variable.cmin).apply(Variable())
-	def max(self): return Function(self, op=Variable.cmax).apply(Variable())
-	def mean(self): return Function(self, op=Variable.cmean).apply(Variable())
-	def avg(self): return Function(self, op=Variable.cavg).apply(Variable())
+	def sum(self): return Function(self, op=self._sum).apply(Variable())
+	def min(self): return Function(self, op=self._min).apply(Variable())
+	def max(self): return Function(self, op=self._max).apply(Variable())
+	def mean(self): return Function(self, op=self._mean).apply(Variable())
+	def avg(self): return Function(self, op=self._avg).apply(Variable())
 
 	# str tenor ops
 	def red(self, buffer:str): return f"\033[92m{buffer}\033[0m"
 	def green(self, buffer:str): return f"\033[91m{buffer}\033[0m"
 	def blue(self, buffer:str): return f"\033[94m{buffer}\033[0m"
+	def pink(self, buffer:str): return f"\033[95m{buffer}\033[0m"
 	
 	def charbuff(self, n:int): return [" " for _ in range(n)]
-	def replace_char(self, _:int, char:str): self.data[_]=char; return self
-	def condition(self, cond:str, op:str): print(0) if exec(cond) else None
-		# exec(op) if exec(cond) else None 
-	def inter_replace(self, n:int, token:str): s=[self.condition(f"{_}%{n}==0", f"self.replace_char({_}, {token})") for _ in range(self.size)];return self
+	def replace_char(self, _:int, char:str): self.data[_].data=char 
+	def inter_replace(self, n:int, token:str): 
+		for _ in range(int(self.size/n)-1): self.replace_char((_+1)*n, token)
+		return self
 
 	def merge(x, y):	
 		data = [x.data[_] + y.data[_] for _ in range(min(x.m, y.m))]
@@ -222,48 +189,32 @@ class Tensor(Node):
 	def _corner(self, _:list): x[0]="┌"; x[-1]="└"; return x
 	def branch(self, i, j): return [self._branch() for _ in range(i, j)]
 
+class Function(Node):
+	def __init__(self, *fxn, op=None): self.data = fxn; self.op = op
+	@property
+	def ret(self): return self.data[-1]
+	@property
+	def name(self): return f"\033[96m{self.op.__name__}\033[0m"
+	def apply(self, ret): 
+		self.data = self.push(self.data, ret) 
+		self.ret.ctx = self
+		if isinstance(self.ret, Tensor): self.ret.permuate(op=self.op, x=self.data[0], y=self.data[1])
+		if isinstance(self.ret, Variable): self.__call__()
+		return self.ret
+	def __call__(self): 
+		self.op(self.data)
+	
+	def __repr__(self): 
+		if (len(self.data)==2): return f"{self.name} ( {self.data[0].id} ) -> {self.data[1].id}"
+		if (len(self.data)==3): return f"{self.name} ( {self.data[0].id} {self.data[1].id} ) -> {self.data[2].id}"
+	def __str__(self): 
+		if (len(self.data)==2): return f"{self.name}( x={self.data[0].id} ):\n{self.data[0]} \n= {self.data[1]}\n"
+		if (len(self.data)==3): return f"{self.name}( x={self.data[0].id}, y={self.data[1].id} ):\n{self.data[0]}, \n{self.data[1]} \n= {self.data[2]}\n"
 
-x = Tensor.strbuff(10, 32)
+
+x = Tensor.str(5, 32)
 print(x)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#x = ord("3")
+# print(x)
 
 
