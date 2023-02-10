f1caa38c26ffaa5e5de78b520719abe678beba30
128x128 <adamzzzhao@gmail.com>
2023-02-10 23:10:55 +1100

    refact

--- include/monolith.h
+++ include/monolith.h
@@ -12,28 +12,6 @@
 #include <termios.h>
 #include <stdarg.h>
 
-uint32_t h32(const char * key) {
-    // MurmurOAAT32 hash
-    uint32_t h = (3323198485ul);
-    for (;*key;++key) {
-	h ^= *key;
-	h *= 0x5bd1e995;
-	h ^= h >> 15;
-    }
-    return h;
-}
-
-uint64_t h64(const char * key) {
-    // MurmurOAAT64 hash
-    uint64_t h = (525201411107845655ull);
-    for (;*key;++key) {
-	h ^= *key;
-	h *= 0x5bd1e9955bd1e995;
-	h ^= h >> 47;
-    }
-    return h;
-}
-
 #include "bits.h"
 #include "util.h"
 #include "types.h"
@@ -41,17 +19,20 @@ uint64_t h64(const char * key) {
 #include "list.h"
 
 
-typedef char       src;
-typedef char       string;
-typedef uint32_t   identifier;
-typedef uint32_t   depth;
-typedef uint32_t   idx;
-typedef uint64_t   hash;
-typedef const char regex;
-typedef const char name;
-typedef const char filename;
-
-typedef struct state state;
+typedef char*            string;
+typedef uint32_t         identifier;
+typedef uint32_t         depth;
+typedef uint32_t         idx;
+typedef uint64_t         hash;
+typedef const char       name;
+typedef const char       filename;
+typedef unsigned char    byte;
+typedef unsigned char    kernel;
+typedef void**           table; 
+typedef uint8_t          u8kernel;
+typedef uint32_t         u32kernel; 
+typedef uint64_t         u64kernel;
+typedef FILE*            fileptr;
 
 typedef enum mlStatus 
 {
@@ -77,82 +58,96 @@ typedef enum scStatus
 scStatus;
 
 
-#define MAX_KERNELS       3 
-#define MAX_KERNEL_BYTES  4096
-#define PROMPT_KERNEL     0
-#define SYMBOL_KERNEL     1
-#define REGEX_KERNEL      2
-#define HASHMAP_SIZE      1024*1024
+#define BUFFER_SIZE       4096
+#define HASHMAP_SIZE      1024
 
-#define STRING            char*
-#define ADDRESS           void*
-#define HASHMAP           void**
-#define BYTES             unsigned char
-
-// monolith
-#define MONOLITH          Monolith 
-
-// kernel
-#define KERNEL(i)         MONOLITH.kernels[i]
-#define KERNEL_BASE(i)    &KERNEL(i).buffer[0]
-#define PROMPT            KERNEL_BASE(PROMPT_KERNEL)
-#define SYMBOL            KERNEL_BASE(SYMBOL_KERNEL)
-#define REGEX             KERNEL_BASE(REGEX_KERNEL)
+#define H32_BASE          3323198485ul
+#define H64_BASE          525201411107845655ull
+#define H32_MUL           0x5bd1e995
+#define H64_MUL           0x5bd1e9955bd1e995
 
 // hashmap
-#define ADDRESS_H(k,h)  h[k%HASHMAP_SIZE]
-#define INSPECT_H(k,h)  *(ADDRESS_H(k, h))
-#define NOTFREE_H(k,h)  INSPECT_H(k,h) ? true : false 
-#define DELETE_H(k,h)   memset(ADDRESS_H(k,h),0,sizeof(ADDRESS))
-#define INSERT_H(k,h,x) NOTFREE_H(k,h) ? ERROR : !*((int*)memcpy(ADDRESS_H(k,h),x,sizeof(ADDRESS)))
+#define DELETE_H(k,h)     memset(ADDRESS_H(k,h),0,sizeof(void*))
+#define INSERT_H(k,h,x)   NOTFREE_H(k,h) ? ERROR : !*((int*)memcpy(ADDRESS_H(k,h),x,sizeof(void*)))
+#define ADDRESS_H(k,h)    h->map[k%HASHMAP_SIZE]
+#define INSPECT_H(k,h)    *(ADDRESS_H(k, h))
+#define NOTFREE_H(k,h)    INSPECT_H(k,h) ? true : false 
 
 
 #define MLSTATUS(x) (x ? RUNNING : (Monolith.status=ERROR)) 
 
-typedef struct kernel 
+typedef struct hashmap 
 {
-    BYTES buffer[MAX_KERNEL_BYTES];
-    idx   current;
+    table          map[HASHMAP_SIZE];
 }
-kernel;
+hashmap;
+
+typedef struct scanner
+{
+    byte           buffer[BUFFER_SIZE]; 
+    scStatus       scanStatus;
+    fileptr        fp;
+}
+scanner;
+
+typedef struct language 
+{
+    byte           symbol[BUFFER_SIZE]; 
+    idx            symbolidx;
+    byte           buffer[BUFFER_SIZE]; 
+    hashmap        symbols;
+    depth          precedence;
+}
+language;
+
 
 typedef struct monolith 
 {
     mlStatus       status;
-    scStatus       scanStatus;
-    kernel         kernels[MAX_KERNELS];
-    FILE*          src;
-    depth          precedence;
-    HASHMAP        symbols[HASHMAP_SIZE];
 } 
 monolith;
 
-monolith Monolith = 
-{
-    RUNNING,
-    NONE,
-};
-
-typedef struct edge 
-{
-    hash   key;
-    state* x;
-    state* y;
-} 
-edge;
-
 typedef struct state 
 {
-    list* edges;
+    list*          edges;
 } 
 state;
+
+typedef struct transition 
+{
+    hash           key;
+    state*         dest;
+} 
+transition;
+
+// global structs
+monolith   Monolith;
+scanner    Scanner;
+language   Language;
+
+uint32_t h32(const char * key) 
+{
+    // MurmurOAAT32 hash
+    uint32_t h = H32_BASE;
+    for (;*key;++key){h^=*key;h*=H32_MUL;h^=h>>15;}
+    return h;
+}
+
+uint64_t h64(const char * key) 
+{
+    // MurmurOAAT64 hash
+    uint64_t h = H64_BASE;
+    for (;*key;++key){h^=*key;h*=H64_MUL;h^=h>>47;}
+    return h;
+}
+
 
 //state* initState(){state* s=(state*)malloc(sizeof(state));s->edges=initList();return s;}
 //edge*  initEdge (){edge* e=(edge*)malloc(sizeof(edge));return e;}
 //void   setStates(edge* e,state* x,state* y){e->x=x,e->y=y;}
 //void   setEdges (edge* e,state* x,state* y){push(x->edges,e);push(y->edges,e);}
 
-int buff2int(STRING b, int n) {
+int buff2int(char* b, int n) {
     int x=0;
     //for(int i=0;i<Monolith.bufferIdx;i++){x+=((Monolith.bufferIdx-i)*10*(Monolith.buffer[i]-'0'));}
     //Monolith.bufferIdx=0;
@@ -172,12 +167,14 @@ void buffpush(char c) {
     //return e;
 //}
 
+// language reconginizer
+
 scStatus scanChar(char c) 
 {
     switch(c) 
     {
-	case '(': Monolith.precedence++; return OPEN;
-	case ')': Monolith.precedence--; return CLOSE;
+	case '(': return OPEN;
+	case ')': return CLOSE;
 	case '|': return ALTERNATION;
 	case '*': return CLOSURE; 
 	case '[': return RANGE;
@@ -186,49 +183,62 @@ scStatus scanChar(char c)
 	default : return NONE;
     }
 }
-scStatus ldsymbol() {return NONE;}
 
-scStatus advance(char c) 
+void pushsymbol(char c) 
 {
-    //state* x = initState();
-    //state* y = initState();
-    //edge*  e = initEdge();
-    //setStates(e,x,y);
-    //setEdges (e,x,y);
-    if (c>='0'&&c<='9') {return NUMBER;}
-    if (c>='A'&&c<='Z') {return UPPER;}
-    if (c>='a'&&c<='z') {return LOWER;}
-    else return scanChar(c);
 }
 
-void regex2NFA(regex* x){while(*x++){Monolith.scanStatus=advance(*x);}}
+void advance(char c) 
+{
+    if (c=='['||c==']') {}
+    if (c>='0'&&c<='9') {}
+    if (c>='A'&&c<='Z') {}
+    if (c>='a'&&c<='z') {}
+    else scanChar(c);
+}
+
+void regex2NFA(string x)
+{
+    while(*x++){advance(*x);}
+}
 
 
 // Main
-void run() { 
-    hd(PROMPT, 32);
-    //regex2NFA("[a-z](b|c)*");
+
+void runScanner() 
+{
+    regex2NFA("a(b|c)*");
+    hd(&Scanner.buffer[0], 32);
 }
 
-void movefp(int n) {fseek(Monolith.src, n, SEEK_CUR);}
-long resetfp() {fseek(Monolith.src, 0, SEEK_SET); return ftell(Monolith.src);}
-long filesize() {fseek(Monolith.src, 0, SEEK_END); return ftell(Monolith.src);}
-void ldpromptkernel(int n) {fread(PROMPT, n, 1, Monolith.src);}
-void runkernel(int n) {ldpromptkernel(n); movefp(n); run();} 
-void runfile() {
-    long n = filesize(); resetfp();
-    for(int i=0;i<floor(n / MAX_KERNEL_BYTES);i++) {
-	runkernel(MAX_KERNEL_BYTES);
+void execute(filename* file)
+{
+    Scanner.fp = fopen(file, "r");
+    while(fgets((char*)&Scanner.buffer[0], BUFFER_SIZE, Scanner.fp)) 
+    {
+	runScanner();
     }
-    runkernel(n % MAX_KERNEL_BYTES);
+    fclose(Scanner.fp);
 }
-void execute(filename* file) {
-    Monolith.src = fopen(file, "r");
-    runfile();
-    fclose(Monolith.src);
+
+void prompt()
+{
+    while(true) 
+    {
+	printf(BLU "> " GRN); 
+	scanf("%s", &Scanner.buffer[0]); 
+	runScanner();
+    }
 }
-void prompt(){while(true){printf(BLU"> "GRN); scanf("%s",PROMPT); run();}}
-void report(int line,char* where,char* msg){printf("[Line: %d] Error %s :%s",line,where,msg);}
-void error(int line,char* msg){report(line, "", msg);}
+
+void report(int line,char* where,char* msg)
+{
+    printf("[Line: %d] Error %s :%s",line,where,msg);
+}
+
+void error(int line,char* msg)
+{
+    report(line, "", msg);
+}
 
 #endif
